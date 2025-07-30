fb08394ea8ca02cfba7dad32606e1f5554b07657
yaoben <2412420275@qq.com>
2025-07-30 03:05:39 -0800

    [CudaArgsProfileRuntime] deal with the kernel ptr arg which is not the base addr of a memory allocated by cudamalloc,etc. provice more accurate addr and size to check func.

--- CudaArgsProfileRuntime/profiler.cpp
+++ CudaArgsProfileRuntime/profiler.cpp
@@ -6,23 +6,6 @@
 #include <vector>
 #include <cstdlib>
 #include <memory>
-#include "json.hpp"
-#include <sys/file.h>
-#include <unistd.h>
-#include <cuda_runtime.h>
-#include <unordered_map>
-#include <dlfcn.h>
-
-using json = nlohmann::json;
-
-#include "profiler.h"
-#include "process_profile.h"
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <cstdlib>
-#include <memory>
 #include <mutex>
 #include "json.hpp"
 #include <sys/file.h>
@@ -113,9 +96,37 @@ static cudaError_t (*original_cudaMallocHost)(void **, size_t) = nullptr;
 static cudaError_t (*original_cudaFreeHost)(void *) = nullptr;
 static cudaError_t (*original_cudaMallocManaged)(void **, size_t, unsigned int) = nullptr;
 
-// global map to store the address and size of the allocated memory
+// 内存区域信息结构
+struct MemoryRegion {
+    void* base_addr;
+    size_t size;
+    std::string type;
+    
+    MemoryRegion(void* addr, size_t sz, const std::string& t) 
+        : base_addr(addr), size(sz), type(t) {}
+    
+    // 检查指针是否在这个内存区域内
+    bool contains(void* ptr) const {
+        return ptr >= base_addr && 
+               ptr < (char*)base_addr + size;
+    }
+    
+    // 计算指针在这个区域内的偏移量
+    size_t getOffset(void* ptr) const {
+        return (char*)ptr - (char*)base_addr;
+    }
+    
+    // 计算从指针位置开始的剩余大小
+    size_t getRemainingSize(void* ptr) const {
+        if (!contains(ptr)) return 0;
+        return size - getOffset(ptr);
+    }
+};
+
+// 存储所有内存区域信息
+std::vector<MemoryRegion> memory_regions;
+// 为了快速查找，也保留原来的map（用于精确匹配）
 std::unordered_map<void*, size_t> memory_map;
-// 记录内存类型（device, host, managed）
 std::unordered_map<void*, std::string> memory_type_map;
 
 // 初始化函数，获取原始CUDA函数指针
@@ -182,6 +193,7 @@ extern "C" cudaError_t cudaMalloc(void **devPtr, size_t size) {
     if (err == cudaSuccess) {
         memory_map[*devPtr] = size;
         memory_type_map[*devPtr] = "device";
+        memory_regions.emplace_back(*devPtr, size, "device");
     }
     return err;
 }
@@ -198,6 +210,14 @@ extern "C" cudaError_t cudaFree(void *devPtr) {
     if (err == cudaSuccess) {
         memory_map.erase(devPtr);
         memory_type_map.erase(devPtr);
+        // 从memory_regions中移除对应的区域
+        memory_regions.erase(
+            std::remove_if(memory_regions.begin(), memory_regions.end(),
+                [devPtr](const MemoryRegion& region) {
+                    return region.base_addr == devPtr;
+                }),
+            memory_regions.end()
+        );
     }
     return err;
 }
@@ -239,6 +259,7 @@ extern "C" cudaError_t cudaMallocHost(void **ptr, size_t size) {
     if (err == cudaSuccess) {
         memory_map[*ptr] = size;
         memory_type_map[*ptr] = "host";
+        memory_regions.emplace_back(*ptr, size, "host");
     }
     return err;
 }
@@ -255,6 +276,14 @@ extern "C" cudaError_t cudaFreeHost(void *ptr) {
     if (err == cudaSuccess) {
         memory_map.erase(ptr);
         memory_type_map.erase(ptr);
+        // 从memory_regions中移除对应的区域
+        memory_regions.erase(
+            std::remove_if(memory_regions.begin(), memory_regions.end(),
+                [ptr](const MemoryRegion& region) {
+                    return region.base_addr == ptr;
+                }),
+            memory_regions.end()
+        );
     }
     return err;
 }
@@ -271,8 +300,19 @@ extern "C" cudaError_t cudaMallocManaged(void **devPtr, size_t size, unsigned in
     if (err == cudaSuccess) {
         memory_map[*devPtr] = size;
         memory_type_map[*devPtr] = "managed";
+        memory_regions.emplace_back(*devPtr, size, "managed");
     }
     return err;
+}
+
+// 查找指针所属的内存区域
+const MemoryRegion* findMemoryRegion(void* ptr) {
+    for (const auto& region : memory_regions) {
+        if (region.contains(ptr)) {
+            return &region;
+        }
+    }
+    return nullptr;
 }
 
 // Forward declaration
@@ -302,9 +342,22 @@ void parse_and_add_value(json& param_info, char* data_addr) {
         void* ptr_value = *reinterpret_cast<void**>(data_addr);
         char hex_buf[20];
         sprintf(hex_buf, "%p", ptr_value);
+        
+        // 首先尝试精确匹配
         if (memory_map.find(ptr_value) != memory_map.end()) {
             param_info["size"] = memory_map[ptr_value];
             param_info["memory_type"] = memory_type_map[ptr_value];
+        } else {
+            // 如果精确匹配失败，尝试查找内存区域
+            const MemoryRegion* region = findMemoryRegion(ptr_value);
+            if (region) {
+                param_info["size"] = region->getRemainingSize(ptr_value);
+                param_info["memory_type"] = region->type;
+                char base_addr_buf[20];
+                sprintf(base_addr_buf, "%p", region->base_addr);
+                param_info["base_address"] = base_addr_buf;
+                param_info["offset"] = region->getOffset(ptr_value);
+            }
         }
         param_info["value"] = hex_buf;
     } else {
